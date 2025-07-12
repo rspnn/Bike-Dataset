5989254618e89d4df287a8634f66b29e28cf28b0
Ahmad Rizky Fadhilah <72900768+rspnn@users.noreply.github.com>
2025-07-12 13:29:18 +0700

    Update dashboard.py

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -4,7 +4,7 @@ import plotly.express as px
 import plotly.graph_objects as go
 
 # Load data
-df = pd.read_csv("./bike.csv")
+df = pd.read_csv("dashboard/bike.csv")
 
 st.set_page_config(page_title="Bike Sharing Dashboard", layout="wide")
 
