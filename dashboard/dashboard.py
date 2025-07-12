552a6e2ce2d7daf4c89c8eb2373623553823e9fa
Ahmad Rizky Fadhilah <72900768+rspnn@users.noreply.github.com>
2025-07-12 12:35:12 +0700

    Update dashboard.py
    add new package

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -1,7 +1,11 @@
-
-import streamlit as st
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
 import plotly.express as px
+sns.set(style='dark')
+
 
 # =======================
 # LOAD DATA
